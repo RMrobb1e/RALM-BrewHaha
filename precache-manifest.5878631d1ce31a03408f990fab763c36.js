330e02ca16463aee3ee8d5fcdf71ec45f62ae78c
Robbie Angelo Mariano <robbiemariano126@gmail.com>
2019-12-09 09:20:49 +0800

    Updates

---
+++ precache-manifest.5878631d1ce31a03408f990fab763c36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed93401b2afebaa909847b16294f79ed",
+    "url": "/RALM-BrewHaha/index.html"
+  },
+  {
+    "revision": "de3c1aafde5507c9583f",
+    "url": "/RALM-BrewHaha/static/css/2.465d1ef4.chunk.css"
+  },
+  {
+    "revision": "7cda1ee79349e4e47e0e",
+    "url": "/RALM-BrewHaha/static/css/main.1a4d6ef6.chunk.css"
+  },
+  {
+    "revision": "de3c1aafde5507c9583f",
+    "url": "/RALM-BrewHaha/static/js/2.0debaf92.chunk.js"
+  },
+  {
+    "revision": "8278387c71dc4631f982339e1dfdcc55",
+    "url": "/RALM-BrewHaha/static/js/2.0debaf92.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7cda1ee79349e4e47e0e",
+    "url": "/RALM-BrewHaha/static/js/main.2feab2da.chunk.js"
+  },
+  {
+    "revision": "717bbc43a190a26a6bbd",
+    "url": "/RALM-BrewHaha/static/js/runtime-main.12695a06.js"
+  }
+]);
